508f102c1b4bbe430c3f70d5247234735d43ae35
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-13 19:31:34 +0200

    changes

--- App.js
+++ App.js
@@ -3,19 +3,19 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./screens/Home";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Details from "./screens/Details";
-import 'react-native-reanimated'
-import 'react-native-gesture-handler'
-import { StatusBar } from 'expo-status-bar';
+import "react-native-reanimated";
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-      <StatusBar style="light" />
+        <StatusBar style="light" />
         <Stack.Navigator
           initialRouteName="Home"
-          screenOptions={{ headerShown: false, presentation:"card" }}
+          screenOptions={{ headerShown: false, presentation: "card" }}
         >
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="Details" component={Details} />
