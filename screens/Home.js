9064dbf8b8cee3a141659dec69fd776e71d4dc0e
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-13 18:15:41 +0200

    changes

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,127 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Feather from "react-native-vector-icons/Feather";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const APIkey = "db5595bf66ed081a4a8bc0aff8227211";
 
 const Home = ({ navigation }) => {
+  const [data, setData] = useState(null);
+  const [location, setLocation] = useState("Lusaka");
+  const [inputValue, setInputValue] = useState("");
+  const [animate, setAnimate] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    try {
+      if (inputValue === "") {
+        setAnimate(true);
+        setTimeout(() => {
+          setAnimate(false);
+        }, 500);
+      }
+      if (inputValue !== "") {
+        setLocation(inputValue);
+      }
+      const input = document.querySelector("input");
+      input.value = "";
+      e.preventDefault();
+    } catch (error) {
+      setErrorMsg("Something went wrong..");
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    try {
+      setLoading(true);
+      setErrorMsg("");
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIkey}`;
+      axios
+        .get(url)
+        .then((res) => {
+          setData(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+          setErrorMsg(err.response.data.message);
+          setTimeout(() => {
+            setErrorMsg("");
+          }, 3000);
+        });
+      setTimeout(() => {
+        setLoading(false);
+      }, 1500);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setErrorMsg("Something went wrong");
+    }
+  }, [location]);
+
+  if (!data) {
+    return (
+      <View className="flex min-h-screen w-full bg-[#101010] items-center justify-center">
+        <ActivityIndicator color="white" size={50} />
+      </View>
+    );
+  }
+
+  let icon;
+
+  switch (data.weather[0].main) {
+    case "Clouds":
+      icon = "user";
+      break;
+    case "Haze":
+      icon = "user";
+      break;
+    case "Rain":
+      icon = "user";
+      break;
+    case "Clear":
+      icon = "user";
+      break;
+    case "Drizzle":
+      icon = "user";
+      break;
+    case "Snow":
+      icon = "user";
+      break;
+    case "Thunderstorm":
+      icon = "user";
+      break;
+  }
+
+  const date = new Date();
   return (
     <SafeAreaView className="bg-[#101010] flex-1 py-4 px-4">
       <View className="flex flex-row justify-between items-center">
         <View>
-          <Text className="font-bold text-white text-xl">Zambia</Text>
-          <Text className="text-gray-500 font-light text-xs pt-1">12 May, Sat</Text>
+          <Text className="font-bold text-white text-xl">{data.name}</Text>
+          <Text className="text-gray-500 font-light text-xs pt-1">
+            {date.getUTCDate()}/{date.getUTCMonth() + 1}/{date.getUTCFullYear()}
+          </Text>
         </View>
-        <TouchableOpacity className="p-2s bg-[#202020] rounded-md flex items-center justify-center">
+        <TouchableOpacity className="p-2 bg-[#202020] rounded-md flex items-center justify-center">
           <Feather name="grid" color="#fff" size={24} />
         </TouchableOpacity>
+      </View>
+      <View className="flex flex-row justify-between w-full items-center">
+        <View className="my-6">
+          <Text className="text-6xl text-white">
+            {parseInt(data.main.temp)}°
+          </Text>
+          <Text className="text-white">{data.weather[0].description}</Text>
+        </View>
+        <View>
+          <Feather name={icon} />
+        </View>
       </View>
     </SafeAreaView>
   );
