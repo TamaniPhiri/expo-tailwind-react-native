7aa7cb6995fa0f011db09996d8522f5fc97b9eaa
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-13 22:01:25 +0200

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,7 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}","./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      keyframes:{
-        shake:{
-          "0%":{
-            transform:'translate(4px,0)',
-          },
-          "50%":{
-            transform:'translate(-4px,0px)'
-          },
-          '100%':{
-            transform:'translate(0,0)'
-          }
-        }
-      },
-    animation:{
-      shake:'shake 150ms 2 linear'
-    }
-    },
+    extend: {},
   },
   plugins: [],
 }
